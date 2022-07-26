daa8635bb149523db1145e70abb699be1b8ea44e
Bill Wen <gang.wen@gmail.com>
2022-07-26 12:47:12 +0800

    Completed Section 4
    Completed Section 4

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Gang Wen on 2022/7/26.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white)
+            }
+            .shadow(radius: 7.0)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
