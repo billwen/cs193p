469475d59689234d8170439dd659b8c13fbeea8f
Bill Wen <gang.wen@gmail.com>
2022-07-27 12:49:30 +0800

    Handling User Input Chapter 2

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    
+    @State
+    private var showFavoriteOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoriteOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
