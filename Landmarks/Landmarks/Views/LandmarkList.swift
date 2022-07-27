fd1f8302ba03d85e077410e562b1594d45f074c3
Bill Wen <gang.wen@gmail.com>
2022-07-27 12:39:40 +0800

    Completed Module 2 Section 8

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -25,6 +25,11 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone8", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName) )
+                .previewDisplayName(deviceName)
+        }
+
     }
 }
