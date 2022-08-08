e52b8665f78730afdfb41c155fe49ffa9125ccb6
Bill Wen <gang.wen@gmail.com>
2022-08-08 19:00:28 +0800

    Generated a new project
    Generated a new project

---
+++ DesignCodeIOS15/DesignCodeIOS15/DesignCodeIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeIOS15App.swift
+//  DesignCodeIOS15
+//
+//  Created by Gang Wen on 2022/8/8.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
