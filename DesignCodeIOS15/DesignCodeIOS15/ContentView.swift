bcc174575e86bcd52312d3574d8e89b519db4c85
Bill Wen <gang.wen@gmail.com>
2022-08-09 10:48:21 +0800

    002_Xcode 13 Visual Editor

--- DesignCodeIOS15/DesignCodeIOS15/ContentView.swift
+++ DesignCodeIOS15/DesignCodeIOS15/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20.0)
+            
+            Text("SwiftUI for iOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("20 sections - 3 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+        }
+        .padding(.all)
+        .frame(height: 350.0)
+        .background(Color("Background"))
+        .cornerRadius(30.0)
+        .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
+
+
     }
 }
 
