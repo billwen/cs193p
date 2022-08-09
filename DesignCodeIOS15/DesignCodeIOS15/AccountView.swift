af737f9d7c6fb6e575365c5b6e57351a41013c44
Bill Wen <gang.wen@gmail.com>
2022-08-09 16:47:45 +0800

    007_List Style and Nvigation View

---
+++ DesignCodeIOS15/DesignCodeIOS15/AccountView.swift
@@ -0,0 +1,33 @@
+//
+//  AccountView.swift
+//  DesignCodeIOS15
+//
+//  Created by Gang Wen on 2022/8/9.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    Text("Settings")
+                    Text("Billing")
+                    Text("Help")
+                }
+                .listRowSeparatorTint(.blue)
+                .listRowSeparator(.hidden)
+
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Account")
+        }
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
