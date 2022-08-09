4824a142f3a9a2f9d83169fb9cadf9f1748cc12d
Bill Wen <gang.wen@gmail.com>
2022-08-09 12:58:46 +0800

    005_Shape, Stroke and Blend Mode

--- DesignCodeIOS15/DesignCodeIOS15/ContentView.swift
+++ DesignCodeIOS15/DesignCodeIOS15/ContentView.swift
@@ -17,6 +17,8 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
                 .cornerRadius(20.0)
+                .padding(9)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
             
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
@@ -38,16 +40,27 @@ struct ContentView: View {
         .padding(.all)
         .padding(.vertical, 20.0)
         .frame(height: 350.0)
-        .background(.ultraThinMaterial)
-        .cornerRadius(30.0)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30.0, style: .continuous))
+        // cornerRadius(30), clipShape(Circle()), mask(Circle().opacity(0.5))
+        // Ellipse, Rectangle, Capsule, RoundRectangle
+//        .mask(RoundedRectangle(cornerRadius: 30.0, style: .continuous))
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+//                .stroke(.black, style: .init(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [100, 20], dashPhase: 10))
+                .stroke(.linearGradient(colors:[.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+                .blendMode(.overlay)
+        )
         .padding(.horizontal, 20.0)
         .background(Image("Blob 1").offset(x: 250, y: -100))
-        .overlay(Image("Illustration 2")
+        .overlay(
+            Image("Illustration 2")
             .resizable()
             .aspectRatio(contentMode: .fit)
             .frame(height: 230)
-            .offset(x: 32, y: -80))
+            .offset(x: 32, y: -80)
+        )
+
 
 
     }
@@ -55,8 +68,12 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .previewDevice("iPhone 13 mini")
+        Group {
+            ContentView()
+                .previewDevice("iPhone 13 mini")
+            ContentView()
+                .previewDevice("iPhone 13 mini")
+        }
         
         ContentView()
             .preferredColorScheme(.dark)
