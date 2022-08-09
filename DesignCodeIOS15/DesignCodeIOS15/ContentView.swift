8d86d396bf14205fd9dcb09bb0546ef4ecc082b3
Bill Wen <gang.wen@gmail.com>
2022-08-09 12:32:33 +0800

    004_Material and Foreground style

--- DesignCodeIOS15/DesignCodeIOS15/ContentView.swift
+++ DesignCodeIOS15/DesignCodeIOS15/ContentView.swift
@@ -21,11 +21,12 @@ struct ContentView: View {
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-            
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+
             Text("20 sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
-                .foregroundColor(.secondary)
+                .foregroundStyle(.secondary)
             
             Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
                 .font(.footnote)
@@ -37,10 +38,16 @@ struct ContentView: View {
         .padding(.all)
         .padding(.vertical, 20.0)
         .frame(height: 350.0)
-        .background(Color("Background"))
+        .background(.ultraThinMaterial)
         .cornerRadius(30.0)
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
         .padding(.horizontal, 20.0)
+        .background(Image("Blob 1").offset(x: 250, y: -100))
+        .overlay(Image("Illustration 2")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(height: 230)
+            .offset(x: 32, y: -80))
 
 
     }
@@ -49,6 +56,10 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 mini")
+        
+        ContentView()
+            .preferredColorScheme(.dark)
             .previewDevice("iPhone 13 mini")
     }
 }
