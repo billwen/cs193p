0939da774c591b6a8b3c4c29bbd6afc698ed03f2
Bill Wen <gang.wen@gmail.com>
2022-08-09 14:30:58 +0800

    006_Reusable Style and Color Schema

--- DesignCodeIOS15/DesignCodeIOS15/ContentView.swift
+++ DesignCodeIOS15/DesignCodeIOS15/ContentView.swift
@@ -16,9 +16,10 @@ struct ContentView: View {
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
-                .cornerRadius(20.0)
+                .cornerRadius(10.0)
                 .padding(9)
                 .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
+                .strokeStyle(cornerRadius: 16.0)
             
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
@@ -45,12 +46,15 @@ struct ContentView: View {
         // Ellipse, Rectangle, Capsule, RoundRectangle
 //        .mask(RoundedRectangle(cornerRadius: 30.0, style: .continuous))
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
-        .overlay(
-            RoundedRectangle(cornerRadius: 30, style: .continuous)
-//                .stroke(.black, style: .init(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [100, 20], dashPhase: 10))
-                .stroke(.linearGradient(colors:[.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
-                .blendMode(.overlay)
-        )
+        .strokeStyle(cornerRadius: 30.0)
+//        .modifier(StrokeStyle())
+//
+//        .overlay(
+//            RoundedRectangle(cornerRadius: 30, style: .continuous)
+////                .stroke(.black, style: .init(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [100, 20], dashPhase: 10))
+//                .stroke(.linearGradient(colors:[.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+//                .blendMode(.overlay)
+//        )
         .padding(.horizontal, 20.0)
         .background(Image("Blob 1").offset(x: 250, y: -100))
         .overlay(
@@ -69,8 +73,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ContentView()
-                .previewDevice("iPhone 13 mini")
             ContentView()
                 .previewDevice("iPhone 13 mini")
         }
