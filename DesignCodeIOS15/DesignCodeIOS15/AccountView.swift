3cb10589770bad235b768fedcb8917b6c50ed0de
Bill Wen <gang.wen@gmail.com>
2022-08-09 19:17:18 +0800

    009_Navigation Link and URL

--- DesignCodeIOS15/DesignCodeIOS15/AccountView.swift
+++ DesignCodeIOS15/DesignCodeIOS15/AccountView.swift
@@ -38,14 +38,46 @@ struct AccountView: View {
                 .padding()
                 
                 Section {
-                    Label("Settings", systemImage: "gear")
-                    Label("Billing", systemImage: "creditcard")
-                    Label("Help", systemImage: "questionmark")
+                    NavigationLink(destination: ContentView()) {
+                        Label("Settings", systemImage: "gear")
+                    }
+                    
+                    NavigationLink{
+                        Text("Billing")
+                    } label: {
+                        Label("Billing", systemImage: "creditcard")
+                    }
+                    
+                    NavigationLink { ContentView() } label:  {
+                        Label("Help", systemImage: "questionmark")
+                    }
                 }
+
                 .listRowSeparatorTint(.blue)
                 .listRowSeparator(.hidden)
+                
+                Section {
+                    Link(destination: URL(string: "https://apple.com")!) {
+                        HStack {
+                            Label("Website", systemImage: "house")
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    
+                    Link(destination: URL(string: "https://youtube.com")!) {
+                        HStack {
+                            Label("YouTube", systemImage: "tv")
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
 
             }
+            .accentColor(.primary)
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
         }
