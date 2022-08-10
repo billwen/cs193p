e30f00b46e7cdbf80fec3a22cf93a9a0c63685bc
Bill Wen <gang.wen@gmail.com>
2022-08-10 10:12:15 +0800

    011_Struture and Toggle

--- DesignCodeIOS15/DesignCodeIOS15/AccountView.swift
+++ DesignCodeIOS15/DesignCodeIOS15/AccountView.swift
@@ -15,92 +15,106 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List {
-                VStack(spacing: 8) {
-                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
-                        .font(.system(size: 28))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(
-                            Image(systemName: "hexagon")
-                                .symbolVariant(.fill)
-                                .foregroundColor(.blue)
-                                .font(.system(size: 200))
-                                .offset(x: -50, y: -100)
-                    )
-                    Text("Meng To")
-                        .font(.title.weight(.semibold))
+                
+                profile
+                
+                menu
+                
+                links
+
+        }
+    }
+    
+    var profile: some View {
+        VStack(spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .font(.system(size: 28))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    Image(systemName: "hexagon")
+                        .symbolVariant(.fill)
+                        .foregroundColor(.blue)
+                        .font(.system(size: 200))
+                        .offset(x: -50, y: -100)
+            )
+            Text("Meng To")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.large)
+                Text("Canada")
+                    .foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    var menu: some View {
+        Section {
+            NavigationLink(destination: ContentView()) {
+                Label("Settings", systemImage: "gear")
+            }
+            
+            NavigationLink{
+                Text("Billing")
+            } label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            
+            NavigationLink { ContentView() } label:  {
+                Label("Help", systemImage: "questionmark")
+            }
+        }
+
+        .listRowSeparatorTint(.blue)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View {
+        Section {
+            if !isDeleted {
+                Link(destination: URL(string: "https://apple.com")!) {
                     HStack {
-                        Image(systemName: "location")
-                            .imageScale(.large)
-                        Text("Canada")
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
                             .foregroundColor(.secondary)
                     }
                 }
-                .frame(maxWidth: .infinity)
-                .padding()
-                
-                Section {
-                    NavigationLink(destination: ContentView()) {
-                        Label("Settings", systemImage: "gear")
+                .swipeActions(edge: .leading, allowsFullSwipe: false) {
+                    Button(action: {
+                        isDeleted = true
+                    }) {
+                        Label("", systemImage: "trash")
+                            .foregroundColor(.red)
                     }
+                    .tint(.red)
                     
-                    NavigationLink{
-                        Text("Billing")
-                    } label: {
-                        Label("Billing", systemImage: "creditcard")
+                    Button { } label: {
+                        Label("Pin", systemImage: "pin")
                     }
-                    
-                    NavigationLink { ContentView() } label:  {
-                        Label("Help", systemImage: "questionmark")
-                    }
-                }
-
-                .listRowSeparatorTint(.blue)
-                .listRowSeparator(.hidden)
-                
-                Section {
-                    if !isDeleted {
-                        Link(destination: URL(string: "https://apple.com")!) {
-                            HStack {
-                                Label("Website", systemImage: "house")
-                                Spacer()
-                                Image(systemName: "link")
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                        .swipeActions(edge: .leading, allowsFullSwipe: false) {
-                            Button(action: {
-                                isDeleted = true
-                            }) {
-                                Label("", systemImage: "trash")
-                                    .foregroundColor(.red)
-                            }
-                            .tint(.red)
-                            
-                            Button { } label: {
-                                Label("Pin", systemImage: "pin")
-                            }
-                            .tint(.yellow)
-                    }
-                    }
-                    
-                    Link(destination: URL(string: "https://youtube.com")!) {
-                        HStack {
-                            Label("YouTube", systemImage: "tv")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                }
-
+                    .tint(.yellow)
             }
-            .accentColor(.primary)
-            .listStyle(.insetGrouped)
-            .navigationTitle("Account")
+            }
+            
+            Link(destination: URL(string: "https://youtube.com")!) {
+                HStack {
+                    Label("YouTube", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
         }
+
+    }
+    .accentColor(.primary)
+    .listStyle(.insetGrouped)
+    .navigationTitle("Account")
     }
 }
 
